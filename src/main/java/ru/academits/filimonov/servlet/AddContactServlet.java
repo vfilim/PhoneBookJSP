8eea4ecd476c56dd8c910f0599ff01a90c2552f1
vfilim <rashovlad@gmail.com>
2021-10-25 20:36:15 +0700

    убрал лишние конвертеры

--- src/main/java/ru/academits/filimonov/servlet/AddContactServlet.java
+++ src/main/java/ru/academits/filimonov/servlet/AddContactServlet.java
@@ -1,23 +1,18 @@
 package ru.academits.filimonov.servlet;
 
 import ru.academits.filimonov.PhoneBook;
-import ru.academits.filimonov.converter.ContactConverter;
 import ru.academits.filimonov.converter.ContactValidationConverter;
 import ru.academits.filimonov.model.Contact;
 import ru.academits.filimonov.service.ContactService;
 import ru.academits.filimonov.service.ContactValidation;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.Writer;
 
 public class AddContactServlet extends HttpServlet {
 
     private ContactService phoneBookService = PhoneBook.phoneBookService;
-    private ContactConverter contactConverter = PhoneBook.contactConverter;
     private ContactValidationConverter contactValidationConverter = PhoneBook.contactValidationConverter;
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
@@ -29,7 +24,6 @@ public class AddContactServlet extends HttpServlet {
             contact.setPhone(req.getParameter("phone"));
 
             ContactValidation contactValidation = phoneBookService.addContact(contact);
-            String contactValidationJson = contactValidationConverter.convertToJson(contactValidation);
             if (!contactValidation.isValid()) {
                 resp.setStatus(500);
             }
